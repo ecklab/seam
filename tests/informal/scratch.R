c3dbe30bcdd91da6125b18e3af605e566d683edf
daviddalpiaz <dalpiaz2@illinois.edu>
2021-03-30 14:06:17 -0500

    setup informal testing and scratch work

--- tests/informal/scratch.R
+++ tests/informal/scratch.R
@@ -1,13 +1,3 @@
-# code below here is meant to be run before the seam shiny app
-
-player_ids = get_player_ids()
-pitches = data.table::fread("../statcast-utils/statcast-all-pitches.csv")
-
-# code below here is meant to be run within the seam shiny app
-
-# code below here is scratch work
-
-
 process_statcast = function(data) {
 
   # find duplicated columns
---
+++ tests/informal/shiny-prep.R
@@ -0,0 +1,2 @@
+player_ids = get_player_ids()
+pitches = data.table::fread("../statcast-utils/statcast-all-pitches.csv")
