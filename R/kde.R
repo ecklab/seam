18e236a44e4bc2b10b230c66dc1fe1e7171f83b5
daviddalpiaz <dalpiaz2@illinois.edu>
2021-07-12 16:54:07 -0500

    add note about range, and increase grid size

--- R/kde.R
+++ R/kde.R
@@ -2,7 +2,8 @@
 # https://stat.ethz.ch/pipermail/r-help/2006-June/107405.html
 # MASS::kde2d
 # ggtern::kde2d.weighted
-kde = function(x, y = NULL, h = NULL, w = NULL, n = 25, lims = c(range(x), range(y)), ...) {
+# TODO: consider making default limits to be: lims = c(-150, 150, -10, 200))
+kde = function(x, y = NULL, h = NULL, w = NULL, n = 100, lims = c(range(x), range(y)), ...) {
 
   if (is.null(y)) {
     return(stats::density(x, ...))
