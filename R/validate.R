df5374ccba5155971b8730e51ed41c7182df1633
daviddalpiaz <dalpiaz2@illinois.edu>
2021-12-05 15:40:22 -0600

    validation helper function

---
+++ R/validate.R
@@ -0,0 +1,33 @@
+check_in_hdrs = function(alpha, pitch, synthetic, plot = FALSE) {
+
+  if (plot) {
+    p = plot_df(df = synthetic, main = "", stadium = "generic")
+    print(p + geom_point(data = pitch, mapping = aes(x = x, y = y, z = NULL), color = "red"))
+  }
+
+  # TODO: pull these programatically from the input df? (slightly slower)
+  # TODO: mirror this in plotting code
+  x_diff = (150 + 150) / 99
+  y_diff = (200 + 30) / 99
+
+  c = x_diff * y_diff
+
+  total_dens = sum(synthetic$z * c)
+  cut = (1 - alpha)
+
+  dens_synth = synthetic %>%
+    dplyr::mutate(z = z / total_dens) %>%
+    dplyr::arrange(z) %>%
+    dplyr::mutate(cumz = cumsum(z * c)) %>%
+    dplyr::arrange(desc(cumz)) %>%
+    dplyr::mutate(hdr = cumz > cut)
+
+  x_pos = which.min(abs(pitch$x - dens_synth$x))
+  y_pos = which.min(abs(pitch$y - dens_synth$y))
+
+  dens_synth %>%
+    dplyr::filter(.data$x == dens_synth$x[x_pos]) %>%
+    dplyr::filter(.data$y == dens_synth$y[y_pos]) %>%
+    dplyr::pull(.data$hdr)
+
+}
