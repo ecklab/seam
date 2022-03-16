53b33f7e6c5f5a70f165ff796ff15d8b589a8417
daviddalpiaz <dalpiaz2@illinois.edu>
2022-03-16 11:59:09 -0500

    avoid re-running seam when changing stadium

--- app.R
+++ app.R
@@ -11,9 +11,8 @@ mlb_teams = readRDS("data/mlb-teams.Rds")
 stadiums = readRDS("data/stadiums.Rds")
 stadium_paths = readRDS("data/stadium-paths.Rds")
 
-shiny_seam_helper = function(b, p, br, pr, stadium = "generic") {
-
-  seam = do_full_seam_matchup(
+shiny_seam_helper = function(b, p, br, pr) {
+  do_full_seam_matchup(
     .batter = lu_b(b_lu, b),
     .pitcher = lu_p(p_lu, p),
     .bip = bip,
@@ -22,16 +21,16 @@ shiny_seam_helper = function(b, p, br, pr, stadium = "generic") {
     .ratio_batter = br,
     .ratio_pitcher = pr
   )
+}
 
+shiny_plot_helper = function(seam, stadium = "generic") {
   p1 = plot_df(seam$seam_df, stadium = stadium, main = "Full SEAM")
   p2 = plot_df(seam$synth_pitcher_df, stadium = stadium, main = "Synthetic Pitcher")
   p3 = plot_df(seam$synth_batter_df, stadium = stadium, main = "Synthetic Batter")
   p4 = plot_df(seam$empirical_df, stadium = stadium, main = "Empirical Matchup")
   p5 = plot_df(seam$empirical_batter_df, stadium = stadium, main = "Empirical Batter")
   p6 = plot_df(seam$empirical_pitcher_df, stadium = stadium, main = "Empirical Pitcher")
-
   list(p1 = p1, p2 = p2, p3 = p3, p4 = p4, p5 = p5, p6 = p6)
-
 }
 
 ui = fluidPage(
@@ -72,27 +71,33 @@ server = function(input, output, session) {
       b = input$batter,
       p = input$pitcher,
       br = input$b_ratio,
-      pr = input$p_ratio,
+      pr = input$p_ratio
+    )
+  })
+
+  plots = reactive({
+    shiny_plot_helper(
+      seam = matchup(),
       stadium = input$stadium
     )
   })
 
   output$plot_full = renderPlot({
-    matchup()$p1
+    plots()$p1
   })
 
   output$plot_comp = renderPlot({
-    gridExtra::grid.arrange(matchup()$p4, matchup()$p2, matchup()$p3, ncol = 3)
+    gridExtra::grid.arrange(plots()$p4, plots()$p2, plots()$p3, ncol = 3)
   })
 
   output$plot_all = renderPlot({
     gridExtra::grid.arrange(
-      matchup()$p1,
-      matchup()$p2,
-      matchup()$p3,
-      matchup()$p4,
-      matchup()$p5,
-      matchup()$p6,
+      plots()$p1,
+      plots()$p2,
+      plots()$p3,
+      plots()$p4,
+      plots()$p5,
+      plots()$p6,
       ncol = 3
     )
   })
