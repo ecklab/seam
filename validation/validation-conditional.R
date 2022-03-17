2ccec6b1b0b0547c8199145d8cf2cb3c0a11163d
daviddalpiaz <dalpiaz2@illinois.edu>
2022-03-16 19:57:37 -0500

    init conditional validation

---
+++ validation/validation-conditional.R
@@ -0,0 +1,116 @@
+library(tidyverse)
+devtools::load_all()
+
+bip = readRDS("data/bip.Rds")
+b_lu = as.data.frame(readRDS("data/b-lu.Rds")) # why does this break as a tibble....??
+p_lu = as.data.frame(readRDS("data/p-lu.Rds")) # why does this break as a tibble....??
+
+# modify pools for validation
+batter_pool = get_batter_pool(bip = bip, year_start = 2017, year_end = 2020)
+pitcher_pool = get_pitcher_pool(bip = bip, year_start = 2017, year_end = 2020)
+
+validate_conditional = function() {
+
+  alpha = c(0.10, 0.25, 0.50, 0.75, 0.90)
+
+  trn = bip %>%
+    filter(game_year <= 2020)
+
+  matchups = bip %>%
+    filter(game_year == 2021) %>%
+    group_by(batter, pitcher) %>%
+    summarise(n = n()) %>%
+    filter(n >= 10) %>%
+    select(-n) %>%
+    filter(batter != 628451) %>% # only 2021 bip (cannot fit to trn)
+    filter(batter != 677551) %>% # only 2021 bip (cannot fit to trn)
+    filter(pitcher != 657093)    # only 2021 bip (cannot fit to trn)
+
+  matchup_results = vector(mode = "list", length = nrow(matchups))
+
+  for (i in 1:nrow(matchups)) {
+
+    batter = matchups[i,]$batter
+    pitcher = matchups[i,]$pitcher
+
+    print(c(batter, pitcher))
+
+    tst = bip %>%
+      filter(game_year > 2020) %>%
+      filter(batter == matchups[i,]$batter) %>%
+      filter(pitcher == matchups[i,]$pitcher)
+
+    in_hdr = array(NA, c(nrow(tst), 3, length(alpha)))
+
+    seam = do_full_seam_matchup(
+      .batter = batter,
+      .pitcher = pitcher,
+      .bip = trn,
+      .batter_pool = batter_pool,
+      .pitcher_pool = pitcher_pool,
+      .ratio_batter = .85,
+      .ratio_pitcher = .85
+    )
+
+    for (j in 1:nrow(tst)) {
+
+      # print(j)
+
+      in_hdr[j, ,] = rbind(
+        check_in_hdrs(
+          alpha = alpha,
+          pitch = tst[j, c("x", "y")],
+          synthetic = seam$seam_df,
+          plot = FALSE
+        ),
+        check_in_hdrs(
+          alpha = alpha,
+          pitch = tst[j, c("x", "y")],
+          synthetic = seam$empirical_pitcher_df,
+          plot = FALSE
+        ),
+        check_in_hdrs(
+          alpha = alpha,
+          pitch = tst[j, c("x", "y")],
+          synthetic = seam$empirical_batter_df,
+          plot = FALSE
+        )
+      )
+
+    }
+
+    result = rbind(
+      colMeans(in_hdr[, , 1], na.rm = TRUE),
+      colMeans(in_hdr[, , 2], na.rm = TRUE),
+      colMeans(in_hdr[, , 3], na.rm = TRUE),
+      colMeans(in_hdr[, , 4], na.rm = TRUE),
+      colMeans(in_hdr[, , 5], na.rm = TRUE)
+    )
+    rownames(result) = c(0.10, 0.25, 0.50, 0.75, 0.90)
+    colnames(result) = c("seam", "batter", "pitcher")
+
+    matchup_results[[i]] = result
+
+  }
+
+  matchup_results
+
+}
+
+# results = validate_conditional()
+Reduce(`+`, results) / length(results)
+
+
+extract_row = function(mat, row) {
+  mat[row, ]
+}
+
+res_050 = dplyr::bind_rows(lapply(results, extract_row, row = 3))
+mean(res_050$seam > 0.50)
+mean(res_050$batter > 0.50)
+mean(res_050$pitcher > 0.50)
+
+res_050_long = tidyr::pivot_longer(res_050, cols = 1:3)
+ggplot(res_050_long) +
+  aes(x = value, fill = name) +
+  geom_density(alpha = 0.2)
