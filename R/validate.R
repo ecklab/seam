8f2fa6b7373d700a57a7daa2cf4fafb7ed276c16
daviddalpiaz <dalpiaz2@illinois.edu>
2022-07-01 15:23:12 -0500

    coverage results for fixed area

--- R/validate.R
+++ R/validate.R
@@ -35,6 +35,41 @@ check_in_hdrs = function(alpha, pitch, synthetic, plot = FALSE) {
 
 }
 
+check_in_top_n = function(n, pitch, synthetic, plot = FALSE) {
+
+  if (plot) {
+    p = plot_df(df = synthetic, main = "", stadium = "generic")
+    print(p + geom_point(data = pitch, mapping = aes(x = x, y = y, z = NULL), color = "red"))
+  }
+
+  # TODO: pull these programatically from the input df? (slightly slower)
+  # TODO: mirror this in plotting code
+  x_diff = (150 + 150) / 99
+  y_diff = (200 + 30) / 99
+
+  c = x_diff * y_diff
+  total_dens = sum(synthetic$z * c)
+
+  dens_synth = synthetic %>%
+    dplyr::mutate(z = z / total_dens) %>%
+    dplyr::arrange(desc(z))
+
+  dens_synth$top_n = c(rep(TRUE, n), rep(FALSE, nrow(dens_synth) - n))
+
+  x_pos = which.min(abs(pitch$x - dens_synth$x))
+  y_pos = which.min(abs(pitch$y - dens_synth$y))
+
+  in_top = dens_synth %>%
+    dplyr::filter(.data$x == dens_synth$x[x_pos]) %>%
+    dplyr::filter(.data$y == dens_synth$y[y_pos]) %>%
+    dplyr::pull(.data$top_n)
+
+  output = in_top
+  names(output) = n
+  return(output)
+
+}
+
 calc_hdr_size = function(alpha, synthetic, plot = FALSE) {
 
   if (plot) {
