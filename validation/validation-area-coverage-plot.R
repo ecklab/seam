92c509034dcfa71db6ab9f38e23223df5e5b9c7a
dje13 <dje13@illinois.edu>
2022-07-12 19:24:19 -0500

    updated plot

--- validation/validation-area-coverage-plot.R
+++ validation/validation-area-coverage-plot.R
@@ -6,6 +6,7 @@ devtools::load_all()
 results_many_n = readRDS(file = "validation/conditional-top-n-cov-n.Rds")
 
 res_tnc_n = t(sapply(results_many_n, colMeans))
+graph_points = c(250, 500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000)
 rownames(res_tnc_n) = graph_points
 res_tnc_n = as.data.frame(res_tnc_n)
 res_tnc_n$n = as.numeric(rownames(res_tnc_n))
@@ -16,4 +17,6 @@ res_tnc_n |>
   aes(x = n, y = value, color = name) +
   geom_point() +
   geom_line() +
+  ylab("conditional coverage") +
+  ggtitle("Conditional coverage vs region size") +
   theme_bw()
