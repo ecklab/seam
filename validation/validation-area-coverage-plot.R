069e5a8f41d15a7431e317c759b8ec3556ad3744
dje13 <dje13@illinois.edu>
2022-08-31 08:47:26 -0500

    updated validation script

--- validation/validation-area-coverage-plot.R
+++ validation/validation-area-coverage-plot.R
@@ -42,23 +42,44 @@ colnames(bar)[1] <- "aseam"
 bar_graph_points = rep(seq(from = 1500, to = 2500, by = 50), each = 195)
 bar = as.data.frame(bar)
 bar$n = unlist(bar_graph_points)
-bar = bar |> tidyr::pivot_longer(cols = c("aseam", "batter", "pitcher"))
+bar = bar |>
+  tidyr::pivot_longer(cols = c("aseam", "batter", "pitcher")) |>
+  mutate(dummy = ifelse(name == "pitcher",1,0))
 
-m <- lm(value ~ n + name, data = bar)
-summary(m)
-par(mfrow = c(2,2))
-plot(m)
 
 set.seed(13)
-baz <- t(replicate(5000, coef(lm(value ~ n + name, data = bar[sample(1:nrow(bar), replace = TRUE), ]))))
+m <- lm(value ~ n + name, data = bar)
+par(mfrow = c(2,2))
+plot(m)
+m2 <- lm(value ~ n + name + n*dummy -dummy, data = bar)
+test_stat <- anova(m, m2)$F[2]
 
-ggplot(as.data.frame(baz), aes(x = namepitcher)) +
-  geom_histogram(bins = 50) +
+test <- replicate(1e4, {bar_boot <- bar[sample(1:nrow(bar), replace = T), ]
+  m <- lm(value ~ n + name, data = bar_boot)
+  m2 <- lm(value ~ n + name + n*dummy -dummy, data = bar_boot)
+  out <- anova(m, m2)
+  out$F[2]
+})
+mean(test > test_stat)
+
+
+set.seed(13)
+baz <- t(replicate(1e4, coef(lm(value ~ n + name,
+  data = bar[sample(1:nrow(bar), replace = T), ]))))
+mean(baz[, 3] > 0)
+mean(baz[, 4] > 0)
+
+qux <- as.data.frame(baz) |>
+  dplyr::select(n, namebatter, namepitcher) |>
+  rename("SEAM vs pitcher" = namepitcher, "SEAM vs batter" = namebatter) |>
+  tidyr::pivot_longer(cols = c("SEAM vs batter", "SEAM vs pitcher"))
+ggplot(qux, aes(x = value)) +
+  geom_density() +
+  geom_vline(xintercept = 0, color = "red", size = 1) +
   theme_minimal() +
-  geom_vline(xintercept = 0, color = "red", size = 1)
-ggplot(as.data.frame(baz), aes(x = namebatter)) +
-  geom_histogram(bins = 50) +
-  theme_minimal() +
-  geom_vline(xintercept = 0, color = "red", size = 1)
+  facet_wrap(~name)
+
+
+
 
 
