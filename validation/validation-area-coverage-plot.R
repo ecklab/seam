30210bfaf24a912b4c89fd53b1471b7a31ee73b1
David Dalpiaz <dalpiaz2@illinois.edu>
2022-09-18 21:02:40 -0500

    update validation results

--- validation/validation-area-coverage-plot.R
+++ validation/validation-area-coverage-plot.R
@@ -1,5 +1,5 @@
 # load packages and seam functions
-library(tidyverse)
+library(ggplot2)
 devtools::load_all()
 
 # load previously calculated results
@@ -10,76 +10,75 @@ graph_points = seq(from = 1500, to = 2500, by = 50)
 rownames(res_tnc_n) = graph_points
 res_tnc_n = as.data.frame(res_tnc_n)
 res_tnc_n$n = as.numeric(rownames(res_tnc_n))
-res_tnc_n = res_tnc_n |> tidyr::pivot_longer(cols = c("seam", "batter", "pitcher"))
+res_tnc_n = res_tnc_n |> tidyr::pivot_longer(cols = c("seam", "batter", "pitcher", "seam_mod", "both"))
 
 res_tnc_n |>
   ggplot() +
-  aes(x = n, y = value, color = name) +
-  geom_point() +
-  geom_line() +
-  xlab("Number of grids") +
-  ylab("Average conditional coverage") +
-  ggtitle("Conditional coverage vs region size") +
-  theme_bw()
+    aes(x = n, y = value, color = name) +
+    geom_point() +
+    geom_line() +
+    xlab("Number of grids") +
+    ylab("Average conditional coverage") +
+    ggtitle("Conditional coverage vs region size") +
+    theme_bw()
 
-
-knitr::kable(round(res_tnc_n %>% spread(name, value), 3), format = "latex")
-
-## standard error calculation
-res_tnc_n_se = t(sapply(results_many_n, FUN = function(xx) apply(xx, 2, function(x) sd(x)/sqrt(length(x))) ))
-res_tnc_n_se
-
-
-## K-S calculations
-foo <- round(res_tnc_n %>% spread(name, value), 3)
-ks.test(unlist(foo[,1]), unlist(foo[,3]))
-ks.test(unlist(foo[,1]), unlist(foo[,2]))
-
-
-## regression analysis
-bar <- do.call(rbind, results_many_n)
-colnames(bar)[1] <- "aseam"
-bar_graph_points = rep(seq(from = 1500, to = 2500, by = 50), each = 195)
-bar = as.data.frame(bar)
-bar$n = unlist(bar_graph_points)
-bar = bar |>
-  tidyr::pivot_longer(cols = c("aseam", "batter", "pitcher")) |>
-  mutate(dummy = ifelse(name == "pitcher",1,0))
-
-
-set.seed(13)
-m <- lm(value ~ n + name, data = bar)
-par(mfrow = c(2,2))
-plot(m)
-m2 <- lm(value ~ n + name + n*dummy -dummy, data = bar)
-test_stat <- anova(m, m2)$F[2]
-
-test <- replicate(1e4, {bar_boot <- bar[sample(1:nrow(bar), replace = T), ]
-  m <- lm(value ~ n + name, data = bar_boot)
-  m2 <- lm(value ~ n + name + n*dummy -dummy, data = bar_boot)
-  out <- anova(m, m2)
-  out$F[2]
-})
-mean(test > test_stat)
-
-
-set.seed(13)
-baz <- t(replicate(1e4, coef(lm(value ~ n + name,
-  data = bar[sample(1:nrow(bar), replace = T), ]))))
-mean(baz[, 3] > 0)
-mean(baz[, 4] > 0)
-
-qux <- as.data.frame(baz) |>
-  dplyr::select(n, namebatter, namepitcher) |>
-  rename("SEAM vs pitcher" = namepitcher, "SEAM vs batter" = namebatter) |>
-  tidyr::pivot_longer(cols = c("SEAM vs batter", "SEAM vs pitcher"))
-ggplot(qux, aes(x = value)) +
-  geom_density() +
-  geom_vline(xintercept = 0, color = "red", size = 1) +
-  theme_minimal() +
-  facet_wrap(~name)
-
-
-
-
-
+# knitr::kable(round(res_tnc_n %>% spread(name, value), 3), format = "latex")
+#
+# ## standard error calculation
+# res_tnc_n_se = t(sapply(
+#   results_many_n,
+#   FUN = function(xx)
+#     apply(xx, 2, function(x)
+#       sd(x) / sqrt(length(x)))
+# ))
+# res_tnc_n_se
+#
+#
+# ## K-S calculations
+# foo <- round(res_tnc_n %>% spread(name, value), 3)
+# ks.test(unlist(foo[,1]), unlist(foo[,3]))
+# ks.test(unlist(foo[,1]), unlist(foo[,2]))
+#
+#
+# ## regression analysis
+# bar <- do.call(rbind, results_many_n)
+# colnames(bar)[1] <- "aseam"
+# bar_graph_points = rep(seq(from = 1500, to = 2500, by = 50), each = 195)
+# bar = as.data.frame(bar)
+# bar$n = unlist(bar_graph_points)
+# bar = bar |>
+#   tidyr::pivot_longer(cols = c("aseam", "batter", "pitcher")) |>
+#   mutate(dummy = ifelse(name == "pitcher",1,0))
+#
+#
+# set.seed(13)
+# m <- lm(value ~ n + name, data = bar)
+# par(mfrow = c(2,2))
+# plot(m)
+# m2 <- lm(value ~ n + name + n*dummy -dummy, data = bar)
+# test_stat <- anova(m, m2)$F[2]
+#
+# test <- replicate(1e4, {bar_boot <- bar[sample(1:nrow(bar), replace = T), ]
+#   m <- lm(value ~ n + name, data = bar_boot)
+#   m2 <- lm(value ~ n + name + n*dummy -dummy, data = bar_boot)
+#   out <- anova(m, m2)
+#   out$F[2]
+# })
+# mean(test > test_stat)
+#
+#
+# set.seed(13)
+# baz <- t(replicate(1e4, coef(lm(value ~ n + name,
+#   data = bar[sample(1:nrow(bar), replace = T), ]))))
+# mean(baz[, 3] > 0)
+# mean(baz[, 4] > 0)
+#
+# qux <- as.data.frame(baz) |>
+#   dplyr::select(n, namebatter, namepitcher) |>
+#   rename("SEAM vs pitcher" = namepitcher, "SEAM vs batter" = namebatter) |>
+#   tidyr::pivot_longer(cols = c("SEAM vs batter", "SEAM vs pitcher"))
+# ggplot(qux, aes(x = value)) +
+#   geom_density() +
+#   geom_vline(xintercept = 0, color = "red", size = 1) +
+#   theme_minimal() +
+#   facet_wrap(~name)
--- validation/validation-area-coverage.R
+++ validation/validation-area-coverage.R
@@ -1,39 +1,39 @@
 # load packages and seam functions
-library(tidyverse)
-library(parallel)
 devtools::load_all()
-setwd("../")
 
 # load data
 bip = readRDS("data/bip.Rds")
-b_lu = as.data.frame(readRDS("data/b-lu.Rds")) # why does this break as a tibble....??
-p_lu = as.data.frame(readRDS("data/p-lu.Rds")) # why does this break as a tibble....??
+b_lu = readRDS("data/b-lu.Rds")
+p_lu = readRDS("data/p-lu.Rds")
 
 # modify pools for validation
 batter_pool  = get_batter_pool(bip = bip, year_start = 2017, year_end = 2020)
 pitcher_pool = get_pitcher_pool(bip = bip, year_start = 2017, year_end = 2020)
 
-trn = bip %>%
-  filter(game_year <= 2020)
+trn = bip |>
+  dplyr::filter(game_year <= 2020)
 
-matchups = bip %>%
-  filter(game_year == 2021) %>%
-  group_by(batter, pitcher) %>%
-  summarise(n = n()) %>%
-  filter(n >= 10) %>%
-  select(-n) %>%
-  filter(batter != 628451) %>% # only 2021 bip (cannot fit to trn)
-  filter(batter != 677551) %>% # only 2021 bip (cannot fit to trn)
-  filter(pitcher != 657093)    # only 2021 bip (cannot fit to trn)
+matchups = bip |>
+  dplyr::filter(game_year == 2021) |>
+  dplyr::group_by(batter, pitcher) |>
+  dplyr::summarise(n = dplyr::n(), .groups = "drop") |>
+  dplyr::filter(n >= 10) |>
+  dplyr::select(-n) |>
+  dplyr::filter(batter != 628451) |> # only 2021 bip (cannot fit to trn)
+  dplyr::filter(batter != 677551) |> # only 2021 bip (cannot fit to trn)
+  dplyr::filter(pitcher != 657093)   # only 2021 bip (cannot fit to trn)
+
+method_list = c("seam", "batter", "pitcher", "seam_mod", "both")
 
 get_top_n_coverage = function(n) {
 
   print(n)
 
-  results = matrix(data = 0, nrow = nrow(matchups), ncol = 3)
+  results = matrix(data = 0, nrow = nrow(matchups), ncol = length(method_list))
 
   for (i in 1:nrow(matchups)) {
 
+    # perform estimation for matchup
     est = do_full_seam_matchup(
       .batter = matchups[i, ]$batter,
       .pitcher = matchups[i, ]$pitcher,
@@ -44,71 +44,53 @@ get_top_n_coverage = function(n) {
       .ratio_pitcher = .85
     )
 
-    tst = bip %>%
-      filter(game_year > 2020) %>%
-      filter(batter == matchups[i,]$batter) %>%
-      filter(pitcher == matchups[i,]$pitcher)
+    # create test set for matchup
+    tst = bip
+    tst = tst[tst$game_year == 2021, ]
+    tst = tst[tst$batter == matchups[i,]$batter, ]
+    tst = tst[tst$pitcher == matchups[i,]$pitcher, ]
 
-    in_top = matrix(NA, nrow = nrow(tst), ncol = 3)
+    # create storage for in-out results per pitch for matchup
+    in_top = matrix(NA, nrow = nrow(tst), ncol = length(method_list))
 
     for (j in 1:nrow(tst)) {
-
       in_top[j, ] = c(
-        check_in_top_n(
-          n = n,
-          pitch = tst[j, c("x", "y")],
-          synthetic = est$seam_df,
-          plot = FALSE
-        ),
-        check_in_top_n(
-          n = n,
-          pitch = tst[j, c("x", "y")],
-          synthetic = est$empirical_pitcher_df,
-          plot = FALSE
-        ),
-        check_in_top_n(
-          n = n,
-          pitch = tst[j, c("x", "y")],
-          synthetic = est$empirical_batter_df,
-          plot = FALSE
-        )
+        check_in_top_n(n = n, pitch = tst[j, c("x", "y")], synthetic = est$seam_df),
+        check_in_top_n(n = n, pitch = tst[j, c("x", "y")], synthetic = est$empirical_pitcher_df),
+        check_in_top_n(n = n, pitch = tst[j, c("x", "y")], synthetic = est$empirical_batter_df),
+        check_in_top_n(n = n, pitch = tst[j, c("x", "y")], synthetic = est$seam_mod_df),
+        check_in_top_n(n = n, pitch = tst[j, c("x", "y")], synthetic = est$empirical_both_df)
       )
-
-      # print(in_top[j, ])
-
     }
-
+    # summarize and store conditional coverage for matchup
     results[i, ] = colMeans(in_top)
-
   }
 
-  colnames(results) = c("seam", "batter", "pitcher")
+  colnames(results) = method_list
   return(results)
 
 }
 
+# results for "all n"
+graph_points = seq(from = 1500, to = 2500, by = 50)
+results_many_n = parallel::mclapply(graph_points, get_top_n_coverage, mc.cores = 8)
 
+# save results for many n as a list
+saveRDS(results_many_n, file = "validation/conditional-top-n-cov-n.Rds")
 
+# # results for some n
+# res_tnc_1000 = get_top_n_coverage(n = 1000) # 10%
+# res_tnc_2000 = get_top_n_coverage(n = 2000) # 20%
+# res_tnc_3000 = get_top_n_coverage(n = 3000) # 30%
+#
+# saveRDS(res_tnc_1000, file = "validation/conditional-top-n-cov-1000.Rds")
+# saveRDS(res_tnc_2000, file = "validation/conditional-top-n-cov-2000.Rds")
+# saveRDS(res_tnc_3000, file = "validation/conditional-top-n-cov-3000.Rds")
+
+# # misc calculations
 # 2.33 * diff(asdf$seam_df$x[1:2]) * 2.33 * diff(unique(asdf$seam_df$y)[1:2]) * 3000
 # 115000 * c(0.50, 0.80)
 # 92000 / (2.33 * diff(asdf$seam_df$x[1:2]) * 2.33 * diff(unique(asdf$seam_df$y)[1:2]))
 # 57500 / (2.33 * diff(asdf$seam_df$x[1:2]) * 2.33 * diff(unique(asdf$seam_df$y)[1:2]))
 # 8 - 10 x points
 # seq(from = 1500, to = 2500, length.out = 10)
-
-# results for "all n"
-# graph_points = c(250, 500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000)
-graph_points = seq(from = 1500, to = 2500, by = 50)
-results_many_n = mclapply(graph_points, get_top_n_coverage, mc.cores = 11)
-
-# save results for many n as a list
-saveRDS(results_many_n, file = "validation/conditional-top-n-cov-n.Rds")
-
-# results for some n
-res_tnc_1000 = get_top_n_coverage(n = 1000) # 10%
-res_tnc_2000 = get_top_n_coverage(n = 2000) # 20%
-res_tnc_3000 = get_top_n_coverage(n = 3000) # 30%
-
-saveRDS(res_tnc_1000, file = "validation/conditional-top-n-cov-1000.Rds")
-saveRDS(res_tnc_2000, file = "validation/conditional-top-n-cov-2000.Rds")
-saveRDS(res_tnc_3000, file = "validation/conditional-top-n-cov-3000.Rds")
